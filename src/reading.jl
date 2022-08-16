989a450fb6dab3e40d5b20b4ac0c88f3d65a8fb8
Alex Seitz <alex.seitz1@gmx.net>
2022-08-16 14:58:14 +0200

    Trim byte-order mark from malformatted UTF-8 files before reading

--- src/reading.jl
+++ src/reading.jl
@@ -1,5 +1,9 @@
 function read_jsonl(path::String)::Vector{LensApplication}
-    open(path, "r") do instream
-        return JSON3.read(instream, Vector{LensApplication}, jsonlines = true)
+    if read(open(path, "r"), Char) != '\ufeff'
+        JSON3.read(open(path, "r"), Vector{LensApplication}, jsonlines = true)
+    else
+        instream = open(path, "r")
+        read(instream, Char)
+        JSON3.read(instream, Vector{LensApplication}, jsonlines = true)
     end
 end
