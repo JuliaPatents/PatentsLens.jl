3c129db3cd670e22686fed3e4f3578884fb68573
jfb-h <61913033+jfb-h@users.noreply.github.com>
2022-11-03 15:07:54 +0100

    more Union{Nothing, ...}

--- src/parties.jl
+++ src/parties.jl
@@ -1,19 +1,19 @@
 struct LensExtractedName # helper type, do not export
-    value::String
+    value::Union{String, Nothing}
 end
 StructTypes.StructType(::Type{LensExtractedName}) = StructTypes.Struct()
 
 """Struct representing a patent applicant in the Lens.org format"""
 struct LensApplicant <: AbstractApplicant
     residence::Union{String, Nothing}
-    extracted_name::LensExtractedName
+    extracted_name::Union{LensExtractedName, Nothing}
 end
 StructTypes.StructType(::Type{LensApplicant}) = StructTypes.Struct()
 
 """Struct representing a patent inventor in the Lens.org format"""
 struct LensInventor <: AbstractInventor
     residence::Union{String, Nothing}
-    extracted_name::LensExtractedName
+    extracted_name::Union{LensExtractedName, Nothing}
 end
 StructTypes.StructType(::Type{LensInventor}) = StructTypes.Struct()
 
