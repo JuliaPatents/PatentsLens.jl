d9e754c8a9f28f894383447c53c17c50d911aa23
Alex Seitz <alex.seitz1@gmx.net>
2023-01-05 12:36:23 +0100

    add deploydocs() again

--- docs/make.jl
+++ docs/make.jl
@@ -5,3 +5,7 @@ makedocs(
     sitename = "PatentsLens.jl",
     format = Documenter.HTML(; prettyurls = get(ENV, "CI", nothing) == "true")
 )
+
+deploydocs(
+    repo = "github.com/JuliaPatents/PatentsLens.jl.git"
+)
