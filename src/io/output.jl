d3b7865ad32bb05ccf6e47e090297f3d4abd56ac
Alex Seitz <alex.seitz1@gmx.net>
2023-01-30 13:04:27 +0100

    Add missing Base.show overrides

--- src/io/output.jl
+++ src/io/output.jl
@@ -9,36 +9,51 @@ function Base.show(io::IO, t::LensFulltext)
 end
 
 function Base.show(io::IO, id::LensDocumentID)
-    date = id.date !== nothing ? id.date : "????-??-??"
-    kind = id.kind !== nothing ? id.kind : "?"
+    date = isnothing(id.date) ? "????-??-??" : id.date
+    kind = isnothing(id.kind) ? "?" : id.kind
     print(io, "$date | $(id.jurisdiction)$(id.doc_number)$(id.kind)")
 end
 
 function Base.show(io::IO, ar::LensApplicationReference)
-    id = ar.lens_id !== nothing ? ar.lens_id : "???-???-???-???-???"
+    id = isnothing(ar.lens_id) ? "???-???-???-???-???" : ar.lens_id
     print(io, "$id | $(ar.document_id)")
 end
 
 function Base.show(io::IO, pc::LensPatentCitation)
-    phase = pc.cited_phase !== nothing ? pc.cited_phase : "???"
-    seq = pc.sequence !== nothing ? pc.sequence : "?"
+    phase = isnothing(pc.cited_phase) ? "???" : pc.cited_phase
+    seq = isnothing(pc.sequence) ? "?" : pc.sequence
     print(io, "$phase $seq: $(pc.patcit)")
 end
 
 function Base.show(io::IO, nc::LensNPLCitation)
     doi = PatentsBase.doi(nc)
-    phase = nc.cited_phase !== nothing ? nc.cited_phase : "???"
-    seq = nc.sequence !== nothing ? nc.sequence : "?"
-    if doi !== nothing
-        print(io, "$phase $seq: https://doi.org/$(doi)")
-    else
+    phase = isnothing(nc.cited_phase) ? "???" : nc.cited_phase
+    seq = isnothing(nc.sequence) ? "?" : nc.sequence
+    if isnothing(doi)
         print(io, "$phase $seq: $(nc.nplcit.text)")
+    else
+        print(io, "$phase $seq: https://doi.org/$(doi)")
     end
 end
 
 Base.show(io::IO, fc::LensForwardCitation) = show(io, fc.ref)
 
+function Base.show(io::IO, a::LensApplicant)
+    c = isnothing(country(a)) ? "??" : country(a)
+    print(io, "Applicant: $(name(a)) ($c)")
+end
+
+function Base.show(io::IO, i::LensInventor)
+    c = isnothing(country(i)) ? "??" : country(i)
+    print(io, "Inventor: $(name(i)) ($c)")
+end
+
 function Base.show(io::IO, a::LensApplication)
     text = "$(a.lens_id) | $(a.date_published) | $(a.jurisdiction)$(a.doc_number)$(a.kind)"
     print(io, text)
+end
+
+function Base.show(io::IO, f::LensFamily)
+    println(io, "LensFamily with $(length(applications(f))) members:")
+    join(io, applications(f), "\n")
 end
